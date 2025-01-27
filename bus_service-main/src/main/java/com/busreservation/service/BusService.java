765202649b87c20ebf2d35f28d8a1086aca49a3d
nishchay <nishchaynilekani@gmail.com>
2025-01-27 15:30:09 +0530

    Started Api gateway

--- bus_service-main/src/main/java/com/busreservation/service/BusService.java
+++ bus_service-main/src/main/java/com/busreservation/service/BusService.java
@@ -13,45 +13,44 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class BusService {
-	
+
 	private BusRepo busRepo;
 	private ReservationClient reservationClient;
-	
-	
-	public Bus addBus(Bus  bus) {
+
+	public Bus addBus(Bus bus) {
 		return busRepo.save(bus);
 	}
-	
-	public List<Bus> fetchAllBus(){
+
+	public List<Bus> fetchAllBus() {
 		return busRepo.findAll();
 	}
-	
+
 	public Bus findBusById(Long id) {
 		return busRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Bus not found with id " + id));
 	}
-	
-	public Bus updateBus(Long id,Bus bus) {
-		Bus existingBus=busRepo.findById(id).orElse(null);
-		
-		if(existingBus==null) {
+
+	public Bus updateBus(Long id, Bus bus) {
+		Bus existingBus = busRepo.findById(id).orElse(null);
+
+		if (existingBus == null) {
 			throw new IllegalArgumentException("Bus not found with ID: " + id);
 		}
 		existingBus.setBusNo(bus.getBusNo());
-	    existingBus.setRouteFrom(bus.getRouteFrom());
-	    existingBus.setRouteTo(bus.getRouteTo());
-	    existingBus.setSeats(bus.getSeats());
-	    existingBus.setDepartureTime(bus.getDepartureTime());
-	    existingBus.setPrice(bus.getPrice());
+		existingBus.setRouteFrom(bus.getRouteFrom());
+		existingBus.setRouteTo(bus.getRouteTo());
+		existingBus.setSeats(bus.getSeats());
+		existingBus.setDepartureTime(bus.getDepartureTime());
+		existingBus.setPrice(bus.getPrice());
 
-	    // Save the updated bus
-	    return busRepo.save(existingBus);
+		// Save the updated bus
+		return busRepo.save(existingBus);
 	}
-	
+
 	public void deleteBus(Long id) {
-        busRepo.deleteById(id);
-    }
-	
-	public List<Reservation> getReservations(){
+		busRepo.deleteById(id);
+	}
+
+	public List<Reservation> getReservations() {
 		return reservationClient.getAllReservation();
 	}
 
